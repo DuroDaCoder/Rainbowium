eda15794ecdfffd9c58720887af2d05be10bc7ea
DuroDaCoder <48152410+DuroDaCoder@users.noreply.github.com>
2022-01-13 15:46:42 +0100

    Delete scripts directory

--- scripts/Run_Bot.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-py -3.9 bot.py
-exit
--- scripts/kill_if_running.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-tasklist /fi "imagename eq RainbowSix.exe" |find ":" > nul & echo R6S isn't running, continuing. > output.txt
-if errorlevel 1 taskkill /f /im "RainbowSix.exe" & echo R6S is running, killing it. > output.txt & echo cloud_wait > cloud_wait.txt
-exit
