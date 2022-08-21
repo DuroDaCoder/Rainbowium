9f29f976154d94e4f97bf102c9245c0dbc1a7af4
Bobino <48152410+DuroDaCoder@users.noreply.github.com>
2022-08-21 10:13:51 +0200

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,5 @@
 @echo off
 py -3.9 bot.py
+IF %ERRORLEVEL% NEQ 0 cls && echo If there was an error with running bot.py script or bot.py window hasn't opened, you may have installed Python *anyversion* (64-bit) version. Please uninstall it, or run this app in virtual environment. && pause && exit
 cls
 exit
